09024a68d46af413b6d0966d69a9ed24ad59c8a6
Noel N. Contreras <noeln.contreras@Noels-MacBook-Air.local>
2019-08-29 17:22:24 -0500

    almost got add button working

--- src/Components/AddGame.js
+++ src/Components/AddGame.js
@@ -35,36 +35,36 @@ export default class AddGame extends Component {
     render () {
         return (
             <section className= "add-container">
-                <div className= "title-genre">
+                <div className="title-genre">
                     <label>Game Title:</label>
-                    <input 
-                    name="gameTitle"
-                    placeholder = "Enter game title" 
-                    onChange={this.handleChange}/>
+                    <input
+                        name="gameTitle"
+                        placeholder="Enter game title"
+                        onChange={this.handleChange} />
                     <br />
                     <label>Genre:</label>
-                    <input 
-                    name="genre"
-                    placeholder = "Enter genre of game" 
-                    onChange={this.handleChange}/>
+                    <input
+                        name="genre"
+                        placeholder="Enter genre of game"
+                        onChange={this.handleChange} />
                 </div>
-                <div className= "add-box">
-                    <button 
-                    id="add-button"
-                    onClick={this.handleClick}>Add Game</button>
+                <div className="add-box">
+                    <button
+                        id="add-button"
+                        onClick={this.handleClick}>Add Game</button>
                 </div>
-                <div className= "developer-trailer">
+                <div className="developer-trailer">
                     <label>Developer:</label>
-                    <input 
-                    name="developer"
-                    placeholder = "Enter game developer" 
-                    onChange={this.handleChange}/>
+                    <input
+                        name="developer"
+                        placeholder="Enter game developer"
+                        onChange={this.handleChange} />
                     <br />
                     <label>Trailer Link:</label>
-                    <input 
-                    name="gameTrailer"
-                    placeholder = "Enter game trailer URL" 
-                    onChange={this.handleChange}/>
+                    <input
+                        name="gameTrailer"
+                        placeholder="Enter game trailer URL"
+                        onChange={this.handleChange} />
                 </div>
             </section>
         )
