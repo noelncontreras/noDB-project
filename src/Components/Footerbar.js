f765707aece6b909b45e8aa802d2c2032761b83b
Noel N. Contreras <noeln.contreras@noels-air.attlocal.net>
2019-09-01 16:23:38 -0500

    delete button needs to work

--- src/Components/Footerbar.js
+++ src/Components/Footerbar.js
@@ -5,7 +5,7 @@ export default function Footerbar() {
     return (
         <footer className= "footer-container">
             <div>
-                <h1>Created by Noel Contreras</h1>
+                <h1 id="zoom-fade">Created by Noel Contreras</h1>
             </div>
         </footer>
     )
